b976b60707e1101b4f78cd4e60ef2f1800634986
auguNa <augustin.nae@gmail.com>
2024-07-30 21:59:13 +0200

    first commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/nae/augustin/s04/t01/n02/S04T01N02AugustinNae/S04T01N02AugustinNaeApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.nae.augustin.s04.t01.n02.S04T01N02AugustinNae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02AugustinNaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02AugustinNaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/nae/augustin/s04/t01/n02/S04T01N02AugustinNae/S04T01N02AugustinNaeApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.nae.augustin.s04.t01.n02.S04T01N02AugustinNae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N02AugustinNaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
